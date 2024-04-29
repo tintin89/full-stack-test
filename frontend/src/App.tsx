24fb03179bcd9effe4fe1b54f193d9f8ac5441d0
Nivaldo <nivalditin89@gmail.com>
2024-04-28 23:10:49 -0700

    adding ui to upload file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const APP_STATUS = {
+  IDLE: "idle",
+  ERROR: "error",
+  READY_UPLOAD: "ready_upload",
+  UPLOADING: "uploading",
+  READY_USAGE: "ready_usage",
+} as const;
+
+const BUTTON_TEXT = {
+  [APP_STATUS.READY_UPLOAD]: "Upload",
+  [APP_STATUS.UPLOADING]: "Uploading...",
+};
+
+type AppStatus = (typeof APP_STATUS)[keyof typeof APP_STATUS];
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [status, setStatus] = useState<AppStatus>(APP_STATUS.IDLE);
+  const [file, setFile] = useState<File | null>(null);
+
+  const showButton =
+    status === APP_STATUS.READY_UPLOAD || status === APP_STATUS.UPLOADING;
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const [file] = event.target.files ?? [];
+    if (file) {
+      setFile(file);
+      setStatus(APP_STATUS.READY_UPLOAD);
+    }
+    console.log(file);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (status !== APP_STATUS.READY_UPLOAD || !file) return;
+    setStatus(APP_STATUS.UPLOADING);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <h4>Challenge CSV and Search</h4>
+        <div>
+          <label>
+            <input
+              disabled={status === APP_STATUS.UPLOADING}
+              onChange={handleInputChange}
+              name="file"
+              type="file"
+              accept=".csv"
+            />
+          </label>
+          {showButton && (
+            <button disabled={status === APP_STATUS.UPLOADING}>
+              {BUTTON_TEXT[status]}
+            </button>
+          )}
+        </div>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
